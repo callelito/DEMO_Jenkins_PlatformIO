43f36a1b9b3a5df99ef4b7bd0d6943035097a0b8
Axel Boldt-Christmas <xmas1915@gmail.com>
2020-04-24 08:29:25 +0200

    Update check.py

--- test/test_scripts/check.py
+++ test/test_scripts/check.py
@@ -1,3 +1,46 @@
+"""
+Communication with arduino running.
+int G = 0;
+int Y = 1;
+int R = 2;
+int O = 11;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(O, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available() > 0) {
+    char data = Serial.read();
+    switch( data ) {
+      case 'G':
+        Serial.println(analogRead(G));
+      break;
+      case 'Y':
+        Serial.println(analogRead(Y));
+      break;
+      case 'R':
+        Serial.println(analogRead(R));
+      break;
+      case 'H':
+        analogWrite(O, 255);
+        Serial.println("Ok");
+      break;
+      case 'M':
+        analogWrite(O, 128);
+        Serial.println("Ok");
+      break;
+      case 'L':
+        analogWrite(O, 0);
+        Serial.println("Ok");
+      break;
+      default:
+        Serial.println("Err");
+    }
+  }                   
+}
+"""
 import serial, time, sys
 arduino = serial.Serial('/dev/ttyACM0', 115200, timeout=.1)
 time.sleep(1)
